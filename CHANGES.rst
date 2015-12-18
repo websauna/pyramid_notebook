ca91feaaed0d349ed07773fbfea84dc7d96dc128
Mikko Ohtamaa <mikko@opensourcehacker.com>
2015-12-19 00:21:53 +0200

    Back to development: 0.1.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog for pyramid_notebook
 ==============================
 
+0.1.2 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.1.1 (2015-12-19)
 ------------------
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2.dev0',
 
     description='Embed IPython Notebook shell on your Pyramid website',
     long_description=long_description,
