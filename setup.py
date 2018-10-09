a973141c84e615bff0e947714888ddfc52eb4eaa
Érico Andrei <ericof@gmail.com>
2018-10-09 10:26:34 +0200

    Set zip_safe and min Python version

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
     ],
     keywords='ipython setuptools development shell uwsgi',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    zip_safe=False,
+    python_requires='>=3.5.2',
     include_package_data=True,
     install_requires=[
         "ipython[notebook]>=7.0.1",
